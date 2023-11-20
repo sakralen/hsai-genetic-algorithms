cd521e43aeb4bd1e7adf82efa091073f76cace69
Sakralen <grigoriy.e.spasov@gmail.com>
2023-11-21 01:04:34 +0300

    added simple crossover

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import sys
 from fileutils import parse_csv
 from salesmangenetic import SalesmanGeneticAlgoritm
 
+from plotutils import plot_locations, plot_restored_route, show_plot
+
 if __name__ == "__main__":
     # locations = parse_csv('csv/eil76.csv')
     # best_route = [int(item) - 1 for row in parse_csv(
@@ -16,5 +18,5 @@ if __name__ == "__main__":
     crossover_prob = float(sys.argv[3])
     mutation_prob = float(sys.argv[4])
 
-    locations = parse_csv('csv/eil76.csv')
+    locations = parse_csv('csv/eil76.csv')[:30]
     SalesmanGeneticAlgoritm(locations, population_size, genereation_max, crossover_prob, mutation_prob).execute()
--- salesmangenetic.py
+++ salesmangenetic.py
@@ -2,7 +2,7 @@ import os
 import random
 import numpy as np
 
-from routeutils import generate_neighbour_route, calc_route_length, restore_route
+from routeutils import generate_neighbour_route, calc_route_length, restore_route, fix_route
 from plotutils import prep_plot, plot_locations, plot_restored_route, save_plot, clear_plot
 
 
@@ -27,13 +27,13 @@ class SalesmanGeneticAlgoritm:
             best_route = min(self.population, key=self.target_func)
             print(f'{i}: {self.target_func(best_route):0.0f}')
 
-            # if (i == 0) or (i == self.generation_max - 1):
-            #     prep_plot(i + 1, self.target_func(best_route))
-            #     plot_locations(self.locations)
-            #     plot_restored_route(restore_route(best_route), self.locations)
-            #     save_plot(self.population_size, self.generation_max,
-            #                 self.crossover_prob, self.mutation_prob, i + 1)
-            #     clear_plot()
+            if (i == 0) or (i == self.generation_max - 1):
+                prep_plot(i + 1, self.target_func(best_route))
+                plot_locations(self.locations)
+                plot_restored_route(restore_route(best_route), self.locations)
+                save_plot(self.population_size, self.generation_max,
+                          self.crossover_prob, self.mutation_prob, i + 1)
+                clear_plot()
 
     # swaps two inner vertices in a random subroute of length 4
     def mutate(self):
@@ -49,7 +49,20 @@ class SalesmanGeneticAlgoritm:
                 route[b] = d
 
     def crossover(self):
-        pass
+        
+        for i in range(0, self.population_size // 2):
+            if random.random() < self.crossover_prob:
+                route_a = self.population[2 * i]
+                route_b = self.population[2 * i + 1]
+                crossover_point = random.randint(1, self.cities_cnt - 2)
+
+                self.population[2 * i] = route_a[:crossover_point] + \
+                    route_b[crossover_point:]
+                self.population[2 * i + 1] = route_b[:crossover_point] + \
+                    route_a[crossover_point:]
+
+                fix_route(self.population[2 * i])
+                fix_route(self.population[2 * i + 1])
 
     def reproduce(self):
         values = np.array(
